de1d54971ff7dad2c323c3cf92e9799eccf55ae4
Bartel Meijers <bartelmeijers@Bartels-MacBook-Pro.local>
2017-09-11 02:30:23 +0200

    First setup main.c

---
+++ WeatherStation/src/main.c
@@ -0,0 +1,26 @@
+/*
+WEATHERSTATION
+SERVERCODE
+Written for ESP32-DEV-V1 by Espressif
+As schoolproject and for private measurement purposes.
+
+Author Bartel Meijers, 09.11.2017
+*/
+
+
+#include <HTTPClient.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+#include <WiFiUdp.h>
+
+
+
+void setup(/* arguments */) {
+  /* code */
+}
+
+
+void loop(/* arguments */) {
+  /* code */
+}
